896844c695266049b9f1a343650bff8e288b8bee
ideal <idealities@gmail.com>
2019-11-26 01:14:16 +0800

    fix error: utf8mb4_bin

--- mysql/master/database-init.sql
+++ mysql/master/database-init.sql
@@ -6,5 +6,5 @@ CREATE TABLE `user` (
   `gmt_modified` datetime NOT NULL DEFAULT '2019-05-05 00:00:00' COMMENT '修改时间',
   `user_type` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8_bin COMMENT='user';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='user';
 
