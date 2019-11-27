2d54ba4a6541523b5bf521c9054c829198e50eb9
ideal <idealities@gmail.com>
2019-11-27 14:39:49 +0800

    update consul config

--- backend-www/config/autoload/consul.php
+++ backend-www/config/autoload/consul.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'uri' => 'http://' . getenv('CONSUL_HOST_IP') . ':8500',
+    'uri' => 'http://' . getenv('CONSUL_CONSUL_SERVER_SERVICE_HOST') . ':'
+            . getenv('CONSUL_CONSUL_SERVER_SERVICE_PORT'),
 ];
