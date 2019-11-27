8d4357b0737e7cc65a731a4752ea3ad0a550e6c6
ideal <idealities@gmail.com>
2019-11-27 22:26:47 +0800

    change date

--- mysql/master/database-init.sql
+++ mysql/master/database-init.sql
@@ -2,8 +2,8 @@ CREATE TABLE `user` (
   `uid` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'uid',
   `name` varchar(128) NOT NULL DEFAULT '',
   `intro` longtext NOT NULL COMMENT 'intro',
-  `gmt_create` datetime NOT NULL DEFAULT '2019-05-05 00:00:00' COMMENT '创建时间',
-  `gmt_modified` datetime NOT NULL DEFAULT '2019-05-05 00:00:00' COMMENT '修改时间',
+  `gmt_create` datetime NOT NULL DEFAULT '2016-12-23 12:00:00' COMMENT '创建时间',
+  `gmt_modified` datetime NOT NULL DEFAULT '2016-12-23 12:00:00' COMMENT '修改时间',
   `user_type` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='user';
